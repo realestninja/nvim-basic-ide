cec279c71f3629dd49c997ecf9145fa804fa1853
Nikhil Nawgiri <the@realest.ninja>
2023-02-06 16:56:23 +0100

    Update nvim-tree mappings

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -61,6 +61,8 @@ nvim_tree.setup {
         { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
         { key = "h", cb = tree_cb "close_node" },
         { key = "v", cb = tree_cb "vsplit" },
+        { key = "s", cb = tree_cb "vsplit" },
+        { key = "i", cb = tree_cb "split" }
       },
     },
   },
