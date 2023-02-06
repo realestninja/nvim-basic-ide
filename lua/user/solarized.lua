909c2e25744a533b998f4e976152c42367791a2f
Nikhil Nawgiri <the@realest.ninja>
2023-02-06 12:12:10 +0100

    Add solarized theme

---
+++ lua/user/solarized.lua
@@ -0,0 +1,25 @@
+-- " Default value is "normal", Setting this option to "high" or "low" does use the
+-- " same Solarized palette but simply shifts some values up or down in order to
+-- " expand or compress the tonal range displayed.
+vim.g.neosolarized_contrast = "normal"
+
+-- " Special characters such as trailing whitespace, tabs, newlines, when displayed
+-- " using ":set list" can be set to one of three levels depending on your needs.
+-- " Default value is "normal". Provide "high" and "low" options.
+vim.g.neosolarized_visibility = "normal"
+
+-- " I make vertSplitBar a transparent background color. If you like the origin
+-- " solarized vertSplitBar style more, set this value to 0.
+vim.g.neosolarized_vertSplitBgTrans = 1
+
+-- " If you wish to enable/disable NeoSolarized from displaying bold, underlined
+-- " or italicized" typefaces, simply assign 1 or 0 to the appropriate variable.
+-- " Default values:
+vim.g.neosolarized_bold = 1
+vim.g.neosolarized_underline = 1
+vim.g.neosolarized_italic = 0
+
+-- " Used to enable/disable "bold as bright" in Neovim terminal. If colors of bold
+-- " text output by commands like `ls` aren't what you expect, you might want to
+-- " try disabling this option. Default value:
+vim.g.neosolarized_termBoldAsBright = 1
