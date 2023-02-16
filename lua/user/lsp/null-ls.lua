22969b60cb53ef49ac43032ec7468737f8dc4fe1
Nikhil Nawgiri <the@realest.ninja>
2023-02-16 16:03:42 +0100

    Add preconfigured eslint  setup

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -3,6 +3,8 @@ if not null_ls_status_ok then
   return
 end
 
+local eslint = require("eslint")
+
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
@@ -22,3 +24,23 @@ null_ls.setup {
     diagnostics.flake8,
   },
 }
+
+eslint.setup({
+  bin = 'eslint', -- or `eslint_d`
+  code_actions = {
+    enable = true,
+    apply_on_save = {
+      enable = true,
+      types = { "directive", "problem", "suggestion", "layout" },
+    },
+    disable_rule_comment = {
+      enable = true,
+      location = "separate_line", -- or `same_line`
+    },
+  },
+  diagnostics = {
+    enable = true,
+    report_unused_disable_directives = false,
+    run_on = "type", -- or `save`
+  },
+})
