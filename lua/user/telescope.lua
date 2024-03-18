1130acf30da6d7e7cfb4ad315bf56f61b32e0636
Nikhil Nawgiri <the@realest.ninja>
2024-03-18 15:40:41 +0100

    Add telescope functionality to open multiple selections at onces

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -5,6 +5,23 @@ end
 
 local actions = require "telescope.actions"
 
+-- select_one_or_multi solution was found here:
+-- https://github.com/nvim-telescope/telescope.nvim/issues/1048#issuecomment-1679797700
+local select_one_or_multi = function(prompt_bufnr)
+  local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+  local multi = picker:get_multi_selection()
+  if not vim.tbl_isempty(multi) then
+    require('telescope.actions').close(prompt_bufnr)
+    for _, j in pairs(multi) do
+      if j.path ~= nil then
+        vim.cmd(string.format('%s %s', 'edit', j.path))
+      end
+    end
+  else
+    require('telescope.actions').select_default(prompt_bufnr)
+  end
+end
+
 telescope.setup {
   defaults = {
     prompt_prefix = " ",
@@ -18,6 +35,7 @@ telescope.setup {
         ["<Up>"] = actions.cycle_history_prev,
         ["<C-j>"] = actions.move_selection_next,
         ["<C-k>"] = actions.move_selection_previous,
+        ["<CR>"] = select_one_or_multi,
       },
     },
   },
