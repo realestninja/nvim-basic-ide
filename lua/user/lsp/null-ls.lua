0c0f5ff9a03b325054da86e241cbc75b68316d96
Nikhil Nawgiri <the@realest.ninja>
2024-08-27 12:19:36 +0200

    Set up null-ls eslint for javascript only

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -24,11 +24,11 @@ null_ls.setup {
     formatting.black.with { extra_args = { "--fast" } },
     formatting.stylua,
     formatting.google_java_format,
-    formatting.eslint_d,
+    formatting.eslint_d.with { filetypes = { "javascript" } },
     formatting.stylelint,
     diagnostics.flake8,
-    diagnostics.eslint,
-    code_actions.eslint_d,
+    diagnostics.eslint.with { filetypes = { "javascript" } },
+    code_actions.eslint_d.with { filetypes = { "javascript" } },
   },
 }
 
